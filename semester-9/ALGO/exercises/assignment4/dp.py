057375a8aa606d39760f6da4e6d309824707863e
albe2669 <albert@risenielsen.dk>
2024-11-07 10:58:18 +0100

    Types

--- semester-9/ALGO/exercises/assignment4/dp.py
+++ semester-9/ALGO/exercises/assignment4/dp.py
@@ -1,31 +1,30 @@
 import itertools
+from typing import final, override
 
 EMPTYSET = "\\emptyset"
 
 
 class Node:
-    value = set()
+    value: set[str] = set()
 
     @staticmethod
-    def get_value_as_set_string(values):
+    def get_value_as_set_string(values: set[str]) -> str:
         if not values:
             return EMPTYSET
         return "\\{" + ", ".join(sorted(list(values))) + "\\}"
 
     @staticmethod
-    def print_dp(first, second, result):
+    def print_dp(first: str, second: str, result: str):
         print(f"DP[{first}][{second}] &= {result} = \\\\")
 
     @staticmethod
-    def get_combs_of_set(values):
-        combs = []
+    def get_combs_of_set(values: set[str]) -> list[set[str]]:
+        combs: list[set[str]] = []
         for i in range(len(values) + 1):
-            combs.extend(list(itertools.combinations(values, i)))
-        combs = [set(comb) for comb in combs]
-
+            combs.extend(map(set, itertools.combinations(values, i)))
         return combs
 
-    def get_combs(self):
+    def get_combs(self) -> list[set[str]]:
         return Node.get_combs_of_set(self.value)
 
     def create_relations(self):
@@ -33,24 +32,30 @@ class Node:
 
 
 class Leaf(Node):
+    @override
     def create_relations(self):
         print("\\intertext{Leaf}")
         Node.print_dp(EMPTYSET, EMPTYSET, "0")
 
 
+@final
 class Introduce(Node):
-    def __init__(self, new, child):
+    new: str
+    child: Node
+
+    def __init__(self, new: str, child: Node):
         self.value = child.value.copy()
         self.value.add(new)
         self.new = new
         self.child = child
 
+    @override
     def create_relations(self):
         print(f"\\intertext{{Introduce {self.new}}}")
 
         child_str = self.get_value_as_set_string(self.child.value)
 
-        relations = []
+        relations: list[tuple[str, str]] = []
         relations.append((EMPTYSET, f"DP[{child_str}][{EMPTYSET}] + 1"))
         relations.append((self.new, f"DP[{child_str}][{EMPTYSET}]"))
 
@@ -60,70 +65,79 @@ class Introduce(Node):
 
         self_str = self.get_value_as_set_string(self.value)
         for relation in relations:
-            Node.print_dp(
-                self_str,
-                relation[0],
-                relation[1]
-            )
+            Node.print_dp(self_str, relation[0], relation[1])
 
 
+@final
 class Forget(Node):
-    def __init__(self, forget, child):
+    forget: str
+    child: Node
+
+    def __init__(self, forget: str, child: Node):
         self.value = child.value.copy()
         self.value.remove(forget)
         self.forget = forget
         self.child = child
 
+    @override
     def create_relations(self):
         print(f"\\intertext{{Forget {self.forget}}}")
         child_str = self.get_value_as_set_string(self.child.value)
 
-        relations = []
+        relations: list[tuple[str, str]] = []
         relations.append(
-            (EMPTYSET, f"min(DP[{child_str}][{EMPTYSET}], DP[{child_str}][{self.forget}])"))
+            (
+                EMPTYSET,
+                f"min(DP[{child_str}][{EMPTYSET}], DP[{child_str}][{self.forget}])",
+            )
+        )
         for value in self.value:
             relations.append((value, f"DP[{child_str}][{value}]"))
 
         self_str = self.get_value_as_set_string(self.value)
         for relation in relations:
-            Node.print_dp(
-                self_str,
-                relation[0],
-                relation[1]
-            )
+            Node.print_dp(self_str, relation[0], relation[1])
 
 
+@final
 class Join(Node):
-    def __init__(self, left, right):
-        self.value = left.value.copy()
-        self.value.union(right.value)
+    left: Node
+    right: Node
+
+    def __init__(self, left: Node, right: Node):
+        self.value = left.value.union(right.value)
         self.left = left
         self.right = right
 
+    @override
     def create_relations(self):
         print("\\intertext{Join}")
         left_str = self.get_value_as_set_string(self.left.value)
         right_str = self.get_value_as_set_string(self.right.value)
         self_str = self.get_value_as_set_string(self.value)
 
-        relations = []
+        relations: list[tuple[str, str]] = []
         relations.append(
-            (EMPTYSET, f"DP[{left_str}][{EMPTYSET}] + DP[{right_str}][{EMPTYSET}] - |{self_str}|"))
+            (
+                EMPTYSET,
+                f"DP[{left_str}][{EMPTYSET}] + DP[{right_str}][{EMPTYSET}] - |{self_str}|",
+            )
+        )
 
         for value in self.value:
             relations.append(
-                (value, f"DP[{left_str}][{value}] + DP[{right_str}][{value}] - (|{self_str}| - 1)"))
+                (
+                    value,
+                    f"DP[{left_str}][{value}] + DP[{right_str}][{value}] - (|{self_str}| - 1)",
+                )
+            )
 
         for relation in relations:
-            Node.print_dp(
-                self_str,
-                relation[0],
-                relation[1]
-            )
+            Node.print_dp(self_str, relation[0], relation[1])
 
 
 if __name__ == "__main__":
-    nodes = []
+    nodes: list[Node] = []
     nodes.append(Leaf())
     nodes.append(Introduce("e", nodes[0]))
     nodes.append(Introduce("g", nodes[1]))
